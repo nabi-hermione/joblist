b6020ce8a4294b466bc876c2deb21af0847e0e3d
nabi-hermione <katediane023@gmail.com>
2026-05-16 15:44:24 +0800

    updated database file

--- job_db.sql
+++ job_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `job_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `job_db`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: job_db
@@ -300,4 +302,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-05-16 15:32:05
+-- Dump completed on 2026-05-16 15:42:32
