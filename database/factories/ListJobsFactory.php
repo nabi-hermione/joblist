47d365b1bf80f97bb71b11123f4299c3bcb8a367
nabi-hermione <katediane023@gmail.com>
2026-05-17 18:54:12 +0800

    Updated code and add admin

--- database/factories/ListJobsFactory.php
+++ database/factories/ListJobsFactory.php
@@ -5,22 +5,10 @@ namespace Database\Factories;
 use App\Models\ListJobs;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-
-/**
- * @extends Factory<ListJobs>
- */
 class ListJobsFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
@@ -29,10 +17,7 @@ class ListJobsFactory extends Factory
             'salary' => fake()->randomFloat(2, 50000, 150000),
         ];
     }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
+    
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
