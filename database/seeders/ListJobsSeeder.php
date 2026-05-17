41174148e6239b6dcd980f852b6cb467d688a0e2
nabi-hermione <katediane023@gmail.com>
2026-05-17 20:36:24 +0800

    Updated DatabaseSeeder

--- database/seeders/ListJobsSeeder.php
+++ database/seeders/ListJobsSeeder.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\DB;
 
 class ListJobsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         DB::table('job_listing')->insert([
